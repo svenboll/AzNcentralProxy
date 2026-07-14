c3a0439dcd9a6a8cd74eef0d14ab684c7dd3d133
Sven Boll <sboll@venta-it.de>
2026-07-14 13:58:53 +0200

    use rest api service to retrieve registrationtoken

--- GetRegistrationToken/run.ps1
+++ GetRegistrationToken/run.ps1
@@ -8,47 +8,40 @@ $serverHost = $ENV:NCentralHostname
 $JWT = $ENV:JWTKey
 $SpecifiedCustomerID = $Request.Query.ID
 
+# Define Output Variables
+$script:outerror = $null
+
 # generating URL and http request body
-$bindingURL = "https://" + $serverHost + "/dms2/services2/ServerEI2?wsdl"
-$CustRestBody = 
-@"
-<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:ei2="http://ei2.nobj.nable.com/">
-    <soap:Header/>
-    <soap:Body>
-        <ei2:customerList>
-            <ei2:username>$null</ei2:username>
-            <ei2:password>$JWT</ei2:password>
-            <ei2:settings>
-                <ei2:key>listSOs</ei2:key>
-                <ei2:value>false</ei2:value>
-            </ei2:settings>
-        </ei2:customerList>
-    </soap:Body>
-</soap:Envelope>
-"@ 
+$authheader = @{}
+$authheader.Add("accept", "application/json")
+$authheader.Add("authorization", "Bearer $($JWT)")
+try {
+    $authresponse = Invoke-RestMethod -Uri "https://$($serverHost)/api/auth/authenticate" -Method POST -Headers $authheader
 
-# Invoke RestMethod to receive CustomerList
-Try {
-    $customerlist = (Invoke-RestMethod -Uri $bindingURL -body $CustRestBody -Method POST).Envelope.body.customerListResponse.return
-}
-Catch {
-    Write-Host "Could not connect: $($_.Exception.Message)"
-    exit
-}
-
-# Set up the "Customers" array, then populate
-$Customers = ForEach ($Entity in $CustomerList) {
-    $CustomerAssetInfo = @{}
-    ForEach ($item in $Entity.Items) { $CustomerAssetInfo[$item.key] = $item.Value }
-    [PSCustomObject]@{
-        ID                = $CustomerAssetInfo["customer.customerid"]
-        RegistrationToken = $CustomerAssetInfo["customer.registrationtoken"]
+    try {
+        $customerheader = @{}
+        $customerheader.Add("accept", "application/json")
+        $customerheader.Add("authorization", "Bearer $($authresponse.tokens.access.token)")
+        $customerresponse = Invoke-RestMethod -Uri "https://$($serverHost)/api/customers/$($SpecifiedCustomerID)/registration-token" -Method GET -Headers $customerheader
+    }
+    catch {
+        $script:outerror = $_.Exception
     }
 }
-$Output = ($Customers | Where-Object { $_.ID -eq $SpecifiedCustomerID }).RegistrationToken
+catch {
+    $script:outerror = $_.Exception
+}
+
+if ($script:outerror) {
+    $outStatusCode = $script:outerror.StatusCode
+    $outBody = $script:outerror.Message
+} else {
+    $outStatusCode = [HttpStatusCode]::OK
+    $outBody = $customerresponse.data.registrationToken
+}
 
 # Associate values to output bindings by calling 'Push-OutputBinding'.
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
-        StatusCode = [HttpStatusCode]::OK
-        Body       = $Output
+        StatusCode = $outStatusCode
+        Body       = $outBody
     })
